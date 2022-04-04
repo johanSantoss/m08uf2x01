e9698d82101d4352a90e67a0047bb2acd9b34445
wasach <74709691+wasach@users.noreply.github.com>
2022-04-04 19:23:47 +0200

    ex1 - done ex2 - done ex3 - done ex5 - done ex9 - done ------------------ ex4 - ex6 - ex7 - ex8 -

--- app/src/main/java/johan/santos/m08uf2x01/ex5/Ejercicio5Activity.kt
+++ app/src/main/java/johan/santos/m08uf2x01/ex5/Ejercicio5Activity.kt
@@ -1,12 +1,148 @@
 package johan.santos.m08uf2x01.ex5
 
+import johan.santos.m08uf2x01.databinding.ActivityEjercicio5Binding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.media.MediaPlayer
+import android.speech.tts.TextToSpeech
+import android.speech.tts.UtteranceProgressListener
+import android.util.Log
 import johan.santos.m08uf2x01.R
+import java.util.*
 
-class Ejercicio5Activity : AppCompatActivity() {
+
+class Ejercicio5Activity : AppCompatActivity(), TextToSpeech.OnInitListener  {
+
+    private lateinit var mp: MediaPlayer
+    private lateinit var binding: ActivityEjercicio5Binding
+    lateinit var tts: TextToSpeech
+    private var posicionAudio = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ejercicio5)
+
+        mp = MediaPlayer.create(this, R.raw.numeros)
+        binding = ActivityEjercicio5Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnStartAudio.setOnClickListener {
+            if (binding.txTextoToVoz.text.toString().equals("")) {
+                Log.d("Ejercicio5Activity", "audio-start")
+                mp.start()
+                Log.d("Ejercicio5Activity", "audio-midd")
+                mp.isLooping = binding.btnLoopAudio.text != "No reproducir en forma circular"
+                Log.d("Ejercicio5Activity", "audio-end")
+            } else parlar()
+        }
+
+        binding.btnPauseAudio.setOnClickListener {
+            if (mp.isPlaying) {
+                // guardamos la posición donde lo paramos
+                posicionAudio = mp.currentPosition
+                mp.pause()
+            }
+        }
+
+        binding.btnContinueAudio.setOnClickListener {
+            if (!mp.isPlaying) {
+                // nos colocamos en la posicion indicada
+                mp.seekTo(posicionAudio)
+                mp.start()
+            }
+        }
+
+        binding.btnStopAudio.setOnClickListener {
+            mp.pause()
+            posicionAudio = 0
+            mp.seekTo(0)
+        }
+
+        binding.btnLoopAudio.setOnClickListener {
+            if (binding.btnLoopAudio.text  == "No reproducir en forma circular") {
+                binding.btnLoopAudio.text = "Reproducir en forma circular"
+                mp.isLooping = true
+            } else{
+                binding.btnLoopAudio.text = "No reproducir en forma circular"
+                mp.isLooping = false
+            }
+        }
+
+        tts = TextToSpeech(this, this)
+
+        val speechListener = object : UtteranceProgressListener() {
+            // https://developer.android.com/reference/kotlin/android/speech/tts/UtteranceProgressListener
+            override fun onStart(p: String?) {
+                binding.btnStartAudio.post {
+                    disableButtons()
+                }
+            }
+
+            override fun onDone(p: String?) {
+                binding.btnStartAudio.post {
+                    enableButtons()
+                }
+            }
+
+            override fun onError(p: String?) {
+                binding.btnStartAudio.post {
+                    enableButtons()
+                }
+            }
+        }
+
+        tts.setOnUtteranceProgressListener(speechListener)
+
+    }
+
+    private fun enableButtons(){
+        binding.btnStartAudio.isEnabled = true
+        binding.btnPauseAudio.isEnabled = true
+        binding.btnContinueAudio.isEnabled = true
+        binding.btnStopAudio.isEnabled = true
+        binding.btnLoopAudio.isEnabled = true
+    }
+
+    private fun disableButtons(){
+        binding.btnStartAudio.isEnabled = false
+        binding.btnPauseAudio.isEnabled = false
+        binding.btnContinueAudio.isEnabled = false
+        binding.btnStopAudio.isEnabled = false
+        binding.btnLoopAudio.isEnabled = false
+    }
+
+    private fun parlar(){
+        Log.d("Ejercicio5Activity", "parlar-start")
+        val text = binding.txTextoToVoz.text
+        Log.d("Ejercicio5Activity", "parlar-mid")
+        // https://developer.android.com/reference/android/speech/tts/TextToSpeech --> constants
+        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, "")
+        Log.d("Ejercicio5Activity", "parlar-end")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mp.release()
+
+        // Shutdown TTS
+        if (tts != null) {
+            tts!!.stop()
+            tts!!.shutdown()
+        }
+    }
+
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            // set US English as language for tts
+            val result = tts!!.setLanguage(Locale.getDefault())
+
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS","The Language specified is not supported!")
+            }
+
+        } else {
+            Log.e("TTS", "Initilization Failed!")
+        }
     }
 }
--- app/src/main/java/johan/santos/m08uf2x01/ex9/Ejercicio9Activity.kt
+++ app/src/main/java/johan/santos/m08uf2x01/ex9/Ejercicio9Activity.kt
@@ -1,12 +1,103 @@
 package johan.santos.m08uf2x01.ex9
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import johan.santos.m08uf2x01.R
+import johan.santos.m08uf2x01.databinding.ActivityEjercicio1Binding
+import johan.santos.m08uf2x01.databinding.ActivityEjercicio9Binding
 
 class Ejercicio9Activity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityEjercicio9Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ejercicio9)
+
+        binding = ActivityEjercicio9Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btn1.setOnClickListener {
+            // Search for restaurants nearby
+            val gmmIntentUri = Uri.parse("geo:0,0?q=restaurants")
+            val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+            mapIntent.setPackage("com.google.android.apps.maps")
+            startActivity(mapIntent)
+        }
+
+        binding.btn2.setOnClickListener {
+            // Search for restaurants in San Francisco
+            val gmmIntentUri =
+                Uri.parse("geo:37.7749,-122.4194?q=restaurants")
+            val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+            mapIntent.setPackage("com.google.android.apps.maps")
+            startActivity(mapIntent)
+        }
+        binding.btn3.setOnClickListener {
+            val gmmIntentUri =
+                Uri.parse("geo:37.7749,-122.4194?z=10&q=restaurants")
+            val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+            mapIntent.setPackage("com.google.android.apps.maps")
+            startActivity(mapIntent)
+        }
+        binding.btn4.setOnClickListener {
+            // specific localization
+            val gmmIntentUri =
+                Uri.parse("geo:0,0?q=1600 Amphitheatre Parkway, Mountain+View, California")
+            val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+            mapIntent.setPackage("com.google.android.apps.maps")
+            startActivity(mapIntent)
+        }
+        binding.btn5.setOnClickListener {
+            // Create a Uri from an intent string. Use the result to create an Intent.
+            val gmmIntentUri = Uri.parse("google.streetview:cbll=46.414382,10.013988")
+
+            // Create an Intent from gmmIntentUri. Set the action to ACTION_VIEW
+            val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+            // Make the Intent explicit by setting the Google Maps package
+            mapIntent.setPackage("com.google.android.apps.maps")
+
+            // Attempt to start an activity that can handle the Intent
+            startActivity(mapIntent)
+        }
+        binding.btn6.setOnClickListener {
+            val gmmIntentUri =
+                Uri.parse("geo:0,0?q=casa")
+            val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+            mapIntent.setPackage("com.google.android.apps.maps")
+            startActivity(mapIntent)
+        }
+        binding.btn7.setOnClickListener {
+            // Display the location of Google, San Francisco using a global plus code.
+            var gmmIntentUri = Uri.parse("http://plus.codes/HX9W+WG3, Terrassa")
+            // Equivalently, define the same location using a local plus code
+            //val gmmIntentUri = Uri.parse("https://plus.codes/QJQ5+XX,San%20Francisco")
+            // Construct and use the Intent as in the examples above
+            val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+            mapIntent.setPackage("com.google.android.apps.maps")
+            startActivity(mapIntent)
+        }
+        binding.btn8.setOnClickListener {
+            val gmmIntentUri =
+                Uri.parse("google.navigation:q=copernic")
+            val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+            mapIntent.setPackage("com.google.android.apps.maps")
+            startActivity(mapIntent)
+        }
+        binding.btn9.setOnClickListener {
+            val gmmIntentUri =
+                Uri.parse("google.navigation:q=8+carrer de torrent del batlle, terrassa, barcelona, spain")
+            val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+            mapIntent.setPackage("com.google.android.apps.maps")
+            startActivity(mapIntent)
+        }
+        binding.btn10.setOnClickListener {
+
+        }
+        binding.btn11.setOnClickListener {
+
+        }
     }
 }
